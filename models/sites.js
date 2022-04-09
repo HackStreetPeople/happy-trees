d7c322cd92fead277a6169894e7c33169d1e5acf
JHessler <jhessler11@gmail.com>
2022-04-09 17:20:54 -0400

    updated routes and csv loader

--- models/sites.js
+++ models/sites.js
@@ -1,5 +1,3 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 
 
 module.exports = function (sequelize, DataTypes) {
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const helmet = require('helmet');
 const PORT = process.env.PORT || 3333;
 const app = express();
 const db = require('./models');
-const csv = require('fast-csv');
-const multer = require('multer')
 const bodyparser = require('body-parser')
 
 app.use(cookieParser());
