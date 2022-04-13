e28e36d8562831dd69111565e1bc4aa069390142
utilrr <gmarkghealy@gmail.com>
2022-04-13 16:08:18 -0400

    mostly fixed map code

--- server.js
+++ server.js
@@ -57,127 +57,61 @@ db.sequelize.sync(syncOptions).then(() => {
     require("./db/seed")(db);
   }
 
-  // Map crap start
+  // Map stuff starts here
 
-  const apiOptions = {
-    // apiKey: "MAP_API_KEY",
-    apiKey: "AIzaSyCplpc0oQkWZMiH7Qhu9RruadMb6PikeuU",
-  };
-
-  // map = new google.maps.Map(document.getElementById("map1"), { ...});
-  // map = new google.maps.Map(document.getElementById("map2"), {...});
-  // map = new google.maps.Map(document.getElementById("map3"), {...});
-  // map = new google.maps.Map(document.getElementById("map4"), {...});
-
-  // const loader = new Loader(apiOptions);
-  // console.log("YOUR LOADER IS NOT DEFINED");
-
-  // loader.load().then(() => {
-  //   console.log("Maps JS API loaded");
-  //   const map = displayMap();
-  //   const markers = addMarkers(map);
-  //   clusterMarkers(map, markers);
-  //   addPanToMarker(map, markers);
-  // });
-
-  function displayMap1() {
-    const mapOptions = {
-      // The map, centered at Banner Farm Mitigation Site
-      center: { lat: 35.350886, lng: -82.556899 },
-      zoom: 15,
-      mapId: "map1",
-      mapTypeId: "terrain",
-    };
-    const mapDiv = document.getElementById("map");
-    return new google.maps.Map(mapDiv, mapOptions);
-  }
-
-  function displayMap2() {
-    const mapOptions = {
-      // The map, centered at East Buffalo Mitigation Site
-      center: { lat: 35.3662, lng: -83.8026 },
-      zoom: 15,
-      mapId: "map2",
-      mapTypeId: "terrain",
-    };
-    const mapDiv = document.getElementById("map");
-    return new google.maps.Map(mapDiv, mapOptions);
-  }
-
-  function displayMap3() {
-    const mapOptions = {
-      // The map, centered at Oak Hill Dairy Mitigation Site
-      center: { lat: 35.40367, lng: -81.35136 },
-      zoom: 15,
-      mapId: "map3",
-      mapTypeId: "terrain",
-    };
-    const mapDiv = document.getElementById("map");
-    return new google.maps.Map(mapDiv, mapOptions);
-  }
-
-  function displayMap4() {
-    const mapOptions = {
-      // The map, centered at Double H Mitigation Site
-      center: { lat: 36.529847, lng: -80.987143 },
-      zoom: 15,
-      mapId: "map4",
-      mapTypeId: "terrain",
-    };
-    const mapDiv = document.getElementById("map");
-    return new google.maps.Map(mapDiv, mapOptions);
-  }
-  // function addMarkers(map) {
-  //   const locations = {
-  //     moogMusic: { lat: 35.60119, lng: -82.555381 },
-  //     cumberlandFalls: { lat: 35.604992, lng: -82.564352 },
-  //     pinballMuseum: { lat: 35.596385, lng: -82.556802 },
-  //     mccormickField: { lat: 35.587536, lng: -82.549452 },
-  //     greyEagle: { lat: 35.587072, lng: -82.564484 },
-  //   };
-  //   const markers = [];
-  //   for (const location in locations) {
-  //     const markerOptions = {
-  //       map: map,
-  //       position: locations[location],
-  //       icon: "./img/custom_pin.png",
-  //     };
-  //     const marker = new google.maps.Marker(markerOptions);
-  //     markers.push(marker);
-  //   }
-  //   return markers;
-  // }
-
-  // function clusterMarkers(map, markers) {
-  //   const clustererOptions = { imagePath: "./img/m" };
-  //   const markerCluster = new MarkerClusterer(map, markers, clustererOptions);
-  // }
-
-  // function addPanToMarker(map, markers) {
-  //   let circle;
-  //   markers.map((marker) => {
-  //     marker.addListener("click", (event) => {
-  //       const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-  //       map.panTo(location);
-  //       if (circle) {
-  //         circle.setMap(null);
-  //       }
-  //       circle = drawCircle(map, location);
-  //     });
+  // function initMap() {
+  //   let lenoir = { lat: 35.9140196, lng: -81.5389849 };
+  //   let map = new google.maps.Map(document.getElementById("map"), {
+  //     zoom: 6,
+  //     center: lenoir,
   //   });
-  // }
 
-  // function drawCircle(map, location) {
-  //   const circleOptions = {
-  //     strokeColor: "#FF0000",
-  //     strokeOpacity: 0.8,
-  //     strokeWeight: 1,
-  //     map: map,
-  //     center: location,
-  //     radius: 800,
-  //   };
-  //   const circle = new google.maps.Circle(circleOptions);
-  //   return circle;
+  //   // ARRAY of multiple markers
+  //   let markers = [
+  //     {
+  //       coords: { lat: 36.529847, lng: -80.987143 },
+  //       content: "<h3>Double H Mitigation Site</h3>",
+  //     },
+
+  //     {
+  //       coords: { lat: 35.350886, lng: -82.556899 },
+  //       content: "<h3>Banner Farm Road Mitigation Site</h3>",
+  //     },
+
+  //     {
+  //       coords: { lat: 35.40367, lng: -81.35136 },
+  //       content: "<h3>Oak Hill Dairy Mitigation Site</h3>",
+  //     },
+
+  //     {
+  //       coords: { lat: 35.3662, lng: -83.8026 },
+  //       content: "<h3>East Buffalo Creek Mitigation Site</h3>",
+  //     },
+  //   ];
+
+  //   // Loop through markers
+  //   for (let i = 0; i < markers.length; i++) {
+  //     // Add the marker w/ this
+  //     addMarker(markers[i]);
+  //   }
+
+  //   function addMarker(props) {
+  //     let marker = new google.maps.Marker({
+  //       position: props.coords,
+  //       map: map,
+  //     });
+
+  //     // Check the content
+  //     if (props.content) {
+  //       let infoWindow = new google.maps.InfoWindow({
+  //         content: props.content,
+  //       });
+
+  //       marker.addListener("click", function () {
+  //         infoWindow.open(map, marker);
+  //       });
+  //     }
+  //   }
   // }
 
   // Map crap end
